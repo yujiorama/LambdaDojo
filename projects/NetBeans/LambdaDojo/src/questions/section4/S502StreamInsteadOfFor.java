ad56045b48b5495323c7db308c6de36904b7ea4b
yujiorama <yujiorama+github@gmail.com>
2015-04-08 17:03:35 +0900

    alternative

--- projects/NetBeans/LambdaDojo/src/questions/section4/S502StreamInsteadOfFor.java
+++ projects/NetBeans/LambdaDojo/src/questions/section4/S502StreamInsteadOfFor.java
@@ -3,6 +3,9 @@ package questions.section4;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
+
 public class S502StreamInsteadOfFor {
 
     public S502StreamInsteadOfFor() {
@@ -12,11 +15,14 @@ public class S502StreamInsteadOfFor {
     private void sum() {
         List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        int sum = 0;
-        for (Integer x : numbers) {
-            sum += x;
-        }
-        System.out.println(sum);
+        // int sum = 0;
+        // for (Integer x : numbers) {
+        //     sum += x;
+        // }
+        // System.out.println(sum);
+        IntSummaryStatistics stats = numbers.stream()
+                                            .collect(Collectors.summarizingInt((x) -> x));
+        System.out.println(stats.getSum());
     }
 
     public static void main(String[] args) {

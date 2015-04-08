4fa32ab57bc7178c3916d9e1c79f603417379109
yujiorama <yujiorama+github@gmail.com>
2015-04-08 16:13:51 +0900

    index for loop to stream and lambda expression

--- projects/NetBeans/LambdaDojo/src/questions/section3/S303ForEachInsteadOfFor.java
+++ projects/NetBeans/LambdaDojo/src/questions/section3/S303ForEachInsteadOfFor.java
@@ -1,14 +1,19 @@
 package questions.section3;
 
+import java.util.stream.IntStream;
+
 public class S303ForEachInsteadOfFor {
     public S303ForEachInsteadOfFor() {
         rewriteSimpleLoop();
     }
     
     private void rewriteSimpleLoop() {
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i);
-        }
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.print(i);
+        // }
+        IntStream.range(0, 10).forEach((i) -> {
+                System.out.print(i);
+        });
         System.out.println();
     }
 

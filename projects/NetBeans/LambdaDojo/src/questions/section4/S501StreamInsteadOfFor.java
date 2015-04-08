e7e657799d0e4b8c268dcd939583de60dab46724
yujiorama <yujiorama+github@gmail.com>
2015-04-08 16:24:20 +0900

    old type for loop to foreach

--- projects/NetBeans/LambdaDojo/src/questions/section4/S501StreamInsteadOfFor.java
+++ projects/NetBeans/LambdaDojo/src/questions/section4/S501StreamInsteadOfFor.java
@@ -11,11 +11,16 @@ public class S501StreamInsteadOfFor {
 
     private void filterEven() {
         List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        for (Integer x : numbers) {
-            if (x % 2 == 0) {
-                System.out.print(x + " ");
-            }
-        }
+        // for (Integer x : numbers) {
+        //     if (x % 2 == 0) {
+        //         System.out.print(x + " ");
+        //     }
+        // }
+        numbers.forEach((x) -> {
+                if (x % 2 == 0) {
+                    System.out.print(x + " ");
+                }
+        });
         System.out.println();
     }
 
